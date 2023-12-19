c2cc2b08a0f9096a61b27f2dfc19fe25615c6315
DavidOrtsac <43810738+DavidOrtsac@users.noreply.github.com>
2023-12-20 02:37:33 +0800

    added forms finally

--- app/page.js
+++ app/page.js
@@ -2,61 +2,45 @@
 
 import { useState, useRef, useEffect } from "react";
 
-function useScript(src) {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [src]);
-
-  return null;
-}
-
 export default function Home() {
-  useScript('https://www.googletagmanager.com/gtag/js?id=G-M9HSPDRDPR');
-
-  useEffect(() => {
-    if (window.gtag) {
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', 'G-M9HSPDRDPR');
-    }
-  }, []);
   const [streamedData, setStreamedData] = useState("");
   const [isGenerating, setIsGenerating] = useState(false);
   const [firstOutputComplete, setFirstOutputComplete] = useState(false);
+  const [secondOutputComplete, setSecondOutputComplete] = useState(false);
   const [userPrompt, setUserPrompt] = useState("");
   const abortController = useRef(null);
   const [isBlurred, setIsBlurred] = useState(false);
-  const [isInputExpanded, setIsInputExpanded] = useState(true);  // New state for input area toggle
-  const [buttonText, setButtonText] = useState("Send");  // New state for button text
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);  // New state for button disabled state
+  const [isInputExpanded, setIsInputExpanded] = useState(true);
+  const [buttonText, setButtonText] = useState("Send");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   const toggleInputArea = () => {
     setIsInputExpanded(!isInputExpanded);
   };
 
+  useEffect(() => {
+    if (secondOutputComplete) { // Changed from firstOutputComplete to secondOutputComplete
+      const script = document.createElement("script");
+      script.src = "//embed.typeform.com/next/embed.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, [secondOutputComplete]); // Changed from firstOutputComplete to secondOutputComplete
 
   const streamResponse = async (response, setResponseData, onStreamStart, onComplete) => {
     const reader = response.body.getReader();
     const decoder = new TextDecoder();
     let data = '';
-  
+
     try {
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
         if (onStreamStart && data === '') {
-          onStreamStart(); // Call this function when the stream starts
+          onStreamStart();
         }
         const chunk = decoder.decode(value, { stream: true });
-        data += chunk.replace(/\n/g, ''); // Replace newlines with <br> for HTML display
+        data += chunk.replace(/\n/g, '');
         setResponseData(data);
       }
       if (onComplete) {
@@ -66,34 +50,35 @@ export default function Home() {
       console.error("Streaming error:", error);
     }
   };
+
   const handleChatSubmit = async (e) => {
     e.preventDefault();
     setIsGenerating(true);
     setStreamedData("");
     setFirstOutputComplete(false);
+    setSecondOutputComplete(false); // Resetting second output state
     setIsBlurred(true);
-    setIsButtonDisabled(true); // Disable button when generation starts
-    setButtonText("Preparing Questions..."); // Update button text for first output
+    setIsButtonDisabled(true);
+    setButtonText("Preparing Questions...");
     abortController.current = new AbortController();
 
     try {
       const generateQuizPrompt = `Convert the following passage into a quiz with 5 questions, four choices each, and the answer appended at the end of each question:\n\n${userPrompt}`;
- 
+
       const quizResponse = await fetch("/api/chat", {
         method: "POST",
         body: JSON.stringify({ prompt: generateQuizPrompt }),
         headers: { "Content-Type": "application/json" },
         signal: abortController.current.signal
       });
-  
+
       if (!quizResponse.ok) {
         throw new Error(`HTTP error! status: ${quizResponse.status}`);
       }
-  
-      // Stream the first response and update streamedData state
+
       await streamResponse(quizResponse, setStreamedData, null, async (quizText) => {
         setFirstOutputComplete(true);
-        setButtonText("Generating Quiz..."); // Update button text for second output
+        setButtonText("Generating Quiz...");
   
         const applyHtmlPrompt = `You are a code writer tasked to generate an HTML worksheet with embedded CSS. Your sole purpose is to write clean, functional code without any comments, explanations, or unnecessary labels. You are not to engage in conversation or provide meta-comments.
 
@@ -112,7 +97,7 @@ export default function Home() {
       Generate the worksheet code immediately below this line. Do not include any other information, and do not speak. DO NOT ENCLOSE THE CODE WITHIN ANYTHING, INCLUDING BACKTICKS OR QUOTATIONS:`;
 
  
-   
+ 
       const htmlResponse = await fetch("/api/chat", {
         method: "POST",
         body: JSON.stringify({ prompt: applyHtmlPrompt }),
@@ -124,14 +109,14 @@ export default function Home() {
         throw new Error(`HTTP error! status: ${htmlResponse.status}`);
       }
 
-      // Stream the second response and reset streamedData state before streaming
-      setStreamedData(""); // Clear the existing data
       await streamResponse(htmlResponse, setStreamedData, () => {
-        setIsBlurred(false); // Disable blur when the second output starts streaming
+        setIsBlurred(false);
+      }, () => {
+        setSecondOutputComplete(true); // Set second output to complete here
       });
       setIsGenerating(false);
-      setIsButtonDisabled(false); // Enable button after generation is done
-      setButtonText("Send"); // Reset button text
+      setIsButtonDisabled(false);
+      setButtonText("Send");
     });
   } catch (error) {
     if (error.name === 'AbortError') {
@@ -139,22 +124,24 @@ export default function Home() {
     } else {
       console.error("Error during fetch:", error);
     }
-    setIsGenerating(false); // Also set isGenerating to false in case of an error
-    setIsBlurred(false); // Also remove the blur effect in case of an error
-    setIsButtonDisabled(false); // Enable button in case of an error
-    setButtonText("Send"); // Reset button tex
+    setIsGenerating(false);
+    setIsBlurred(false);
+    setIsButtonDisabled(false);
+    setButtonText("Send");
   }
 };
-  const handleCancel = () => {
-    if (abortController.current) {
-      abortController.current.abort();
-      setIsGenerating(false);
-      setFirstOutputComplete(false);
-      setIsBlurred(false);
-      setIsButtonDisabled(false);  // Enable the button
-      setButtonText("Send");       // Reset the button text
-    }
-  };
+
+const handleCancel = () => {
+  if (abortController.current) {
+    abortController.current.abort();
+    setIsGenerating(false);
+    setFirstOutputComplete(false);
+    setSecondOutputComplete(false);
+    setIsBlurred(false);
+    setIsButtonDisabled(false);
+    setButtonText("Send");
+  }
+};
 
 const handleInputChange = (e) => {
   setUserPrompt(e.target.value);
@@ -162,66 +149,67 @@ const handleInputChange = (e) => {
 
 const handleExampleText = async (e) => {
   e.preventDefault();
-  const fileNames = ['AI.txt', 'DigitalMarketing.txt', 'HoneyBees.txt', 'QuantumComputing.txt', 'SolarSystem.txt'];
-  const randomIndex = Math.floor(Math.random() * fileNames.length);
-  const fileName = fileNames[randomIndex];
+    const fileNames = ['AI.txt', 'DigitalMarketing.txt', 'HoneyBees.txt', 'QuantumComputing.txt', 'SolarSystem.txt'];
+    const randomIndex = Math.floor(Math.random() * fileNames.length);
+    const fileName = fileNames[randomIndex];
 
-  try {
-    const response = await fetch(`/prompts/${fileName}`);
-    const text = await response.text();
-    setUserPrompt(text);
-  } catch (error) {
-    console.error("Error fetching example text:", error);
-  }
-};
+    try {
+      const response = await fetch(`/prompts/${fileName}`);
+      const text = await response.text();
+      setUserPrompt(text);
+    } catch (error) {
+      console.error("Error fetching example text:", error);
+    }
+  };
 
-return (
-  <main className="bg-white min-h-screen p-4 flex justify-center items-center">
-    <div className="max-w-4xl w-full">
-      <div className="text-center mb-8">
-        <h1 className="text-5xl font-bold main-title">The Self-Quiz Engine</h1>
-        <p className="text-xl mt-2">by David Castro</p>
-      </div>
-
-      {!isGenerating && !streamedData && (
-        <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
-          <form onSubmit={handleChatSubmit} className="space-y-4">
-            <textarea
-              className="textarea w-full p-4 border-2 border-gray-300 rounded-lg resize-y overflow-auto"
-              placeholder="Paste your story/essay/report here..."
-              value={userPrompt}
-              onChange={handleInputChange}
-              rows="6"
-              maxLength="14000"
-            />
-            <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-600">{userPrompt.length}/14000</span>
-              <button
-                type="button"
-                onClick={handleExampleText}
-                className="px-4 py-2 bg-gray-200 text-black rounded hover:bg-gray-300 transition duration-200"
-                disabled={isGenerating}
-              >
-                Use Example
-              </button>
-              <button
-                type="submit"
-                className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-700 transition duration-200"
-                disabled={isGenerating || userPrompt.trim() === ''}
-              >
-                Generate
-              </button>
-            </div>
-          </form>
+  return (
+    <main className="bg-white min-h-screen p-4 flex justify-center items-center">
+      <div className="max-w-4xl w-full">
+        <div className="text-center mb-8">
+          <h1 className="text-5xl font-bold main-title">The Self-Quiz Engine</h1>
+          <p className="text-xl mt-2">by David Castro</p>
         </div>
-      )}
-      
-      <div className={`output-container ${isBlurred ? 'grayed-out' : ''}`}>
-  {streamedData && (
-    <div dangerouslySetInnerHTML={{ __html: streamedData }}></div>
-  )}
-</div>
-{(isGenerating || streamedData) && (
+
+        {!isGenerating && !streamedData && (
+          <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+            <form onSubmit={handleChatSubmit} className="space-y-4">
+              <textarea
+                className="textarea w-full p-4 border-2 border-gray-300 rounded-lg resize-y overflow-auto"
+                placeholder="Paste your story/essay/report here..."
+                value={userPrompt}
+                onChange={handleInputChange}
+                rows="6"
+                maxLength="14000"
+              />
+              <div className="flex justify-between items-center">
+                <span className="text-sm text-gray-600">{userPrompt.length}/14000</span>
+                <button
+                  type="button"
+                  onClick={handleExampleText}
+                  className="px-4 py-2 bg-gray-200 text-black rounded hover:bg-gray-300 transition duration-200"
+                  disabled={isGenerating}
+                >
+                  Use Example
+                </button>
+                <button
+                  type="submit"
+                  className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-700 transition duration-200"
+                  disabled={isGenerating || userPrompt.trim() === ''}
+                >
+                  Generate
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+
+        <div className={`output-container ${isBlurred ? 'grayed-out' : ''}`}>
+          {streamedData && (
+            <div dangerouslySetInnerHTML={{ __html: streamedData }}></div>
+          )}
+        </div>
+
+        {(isGenerating || streamedData) && (
           <div className="fixed bottom-4 left-4 right-4 bg-white shadow-lg rounded-lg p-4 flex justify-between items-center">
             {isInputExpanded && (
               <>
@@ -233,14 +221,14 @@ return (
                   onChange={handleInputChange}
                   disabled={isGenerating}
                 />
-          <button
-            type="button"
-            onClick={handleChatSubmit}
-            className={`ml-4 px-6 py-2 rounded-md transition duration-200 ${isButtonDisabled ? 'bg-gray-400' : 'bg-black text-white hover:bg-gray-700'}`}
-            disabled={isButtonDisabled || userPrompt.trim() === ''}
-          >
-            {buttonText}
-          </button>
+                <button
+                  type="button"
+                  onClick={handleChatSubmit}
+                  className={`ml-4 px-6 py-2 rounded-md transition duration-200 ${isButtonDisabled ? 'bg-gray-400' : 'bg-black text-white hover:bg-gray-700'}`}
+                  disabled={isButtonDisabled || userPrompt.trim() === ''}
+                >
+                  {buttonText}
+                </button>
                 {isGenerating && (
                   <button
                     type="button"
@@ -257,6 +245,22 @@ return (
             </div>
           </div>
         )}
+
+<hr></hr>
+
+{secondOutputComplete && (
+          <div className="text-center mt-8 mb-4">
+            <h2 className="text-2xl font-bold">Get notified of weekly app updates</h2>
+            <p className="text-md mt-1">You don’t want to miss the chance to get smarter.</p>
+          </div>
+        )}
+        {secondOutputComplete && (
+          
+          <div className="flex justify-center mb-32">
+            <div data-tf-live="01HHWJ49QMHTPZW7Z45W6CKXER"></div>
+          </div>
+        )}
+
       </div>
     </main>
   );
