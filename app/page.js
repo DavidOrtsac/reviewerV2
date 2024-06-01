db63ce00763c26fcf534272177983ea76298dd56
DavidOrtsac <43810738+DavidOrtsac@users.noreply.github.com>
2024-06-01 13:31:26 +0800

    changed changelog date

--- app/page.js
+++ app/page.js
@@ -425,7 +425,7 @@ const toggleDarkMode = () => {
     <footer className={`${isGenerating ? 'hidden' : ''} fixed bottom-0 left-0 right-0 z-1000 ${darkMode ? 'bg-black text-white' : 'bg-white text-gray-500'} text-center text-sm p-4`}>
         &copy; {new Date().getFullYear()} David Castro. All rights reserved.
         <br></br>
-        <a href="https://calver.org" target="_blank" rel="noopener noreferrer">Release 2024.5.26</a>
+        <a href="https://calver.org" target="_blank" rel="noopener noreferrer">Release 2024.5.31</a>
         <br></br>
         <a href="https://discord.gg/5rDWAzWunK" target="_blank" rel="noopener noreferrer" className="discord-button" style={{ visibility: loaded ? 'visible' : 'hidden' }}>
           <FontAwesomeIcon icon={faDiscord} /> Discord
