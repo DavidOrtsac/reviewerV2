98929bacc957e7d30f64b654186240c5600d15c5
DavidOrtsac <43810738+DavidOrtsac@users.noreply.github.com>
2024-10-21 23:21:51 +0800

    MASSIVE performance boost

--- pages/api/chat/index.js
+++ pages/api/chat/index.js
@@ -1,23 +1,12 @@
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { HumanChatMessage } from "langchain/schema";
+// index.js
+
 import { ChatGroq } from "@langchain/groq";
+import { HumanChatMessage } from "langchain/schema";
 
 export default async function handler(req, res) {
   const { prompt } = req.body;
 
-  // Initialize the model for GPT-4o
-  const gpt4Model = new ChatOpenAI({
-    modelName: 'gpt-4o-mini', // Assuming 'gpt-4' is the correct model name
-    maxTokens: 2000,
-    streaming: true, // Enable streaming for GPT-4 as well
-  });
-
-  const gpt35Model = new ChatOpenAI({
-    modelName: 'gpt-3.5-turbo-16k',
-    maxTokens: 2000,
-    streaming: true,
-  });
-
+  // Initialize your GROQ models
   const groqModel = new ChatGroq({
     model: "llama3-70b-8192",
     maxTokens: 2000,
@@ -31,34 +20,18 @@ export default async function handler(req, res) {
   });
 
   try {
-    // Determine if the prompt is for quiz generation or HTML application
-    if (prompt.startsWith("Convert the following passage into a quiz")) {
-      // Handle quiz generation with GPT-4 and stream the response
-      await groqModel.call([new HumanChatMessage(prompt)], {
-        callbacks: [
-          {
-            handleLLMNewToken(token) {
-              // Streaming quiz response
-              res.write(token);
-            },
+    // Use your preferred GROQ model for quiz generation
+    await groqModel.call([new HumanChatMessage(prompt)], {
+      callbacks: [
+        {
+          handleLLMNewToken(token) {
+            // Streaming quiz response
+            res.write(token);
           },
-        ],
-      });
-      res.end(); // End the response after streaming the quiz content
-    } else {
-      // Handle HTML application with GPT 3.5 and stream the response
-      await gpt4Model.call([new HumanChatMessage(prompt)], {
-        callbacks: [
-          {
-            handleLLMNewToken(token) {
-              // Streaming HTML response
-              res.write(token);
-            },
-          },
-        ],
-      });
-      res.end(); // End the response after streaming the HTML content
-    }
+        },
+      ],
+    });
+    res.end(); // End the response after streaming the quiz content
   } catch (error) {
     console.error("Error in processing:", error);
     res.status(500).send("An error occurred while processing the request.");
